de843d93c67feac6974512dffe7ba85cbf858ad1
lanyuzx <lanyuzx@qq.com>
2020-03-25 17:24:04 +0800

    首页搭建布局

--- app/src/main/java/com/lingyun/yanxuan/classes/home/HomeFragement.java
+++ app/src/main/java/com/lingyun/yanxuan/classes/home/HomeFragement.java
@@ -1,9 +1,18 @@
 package com.lingyun.yanxuan.classes.home;
 
+import android.widget.EditText;
+import android.widget.TextView;
+
 import com.lingyun.yanxuan.R;
 import com.lingyun.yanxuan.base.BaseFragement;
+import com.lingyun.yanxuan.classes.home.bean.HomeBean;
+
+import androidx.appcompat.widget.SearchView;
+import butterknife.BindView;
 
 public class HomeFragement  extends BaseFragement {
+//    @BindView(R.id.home_search_view)
+//    SearchView mEditText;
     @Override
     protected int getLayoutId() {
         return R.layout.fragment_home;
---
+++ app/src/main/java/com/lingyun/yanxuan/classes/home/bean/HomeBean.java
@@ -0,0 +1,14 @@
+package com.lingyun.yanxuan.classes.home.bean;
+
+import java.util.List;
+import java.util.Map;
+
+public class HomeBean {
+
+    private Map commonConfigModule;
+    private List kingKongAreaV4;
+    private Map operationCfg;
+    private List hotStyleList;
+
+}
+
