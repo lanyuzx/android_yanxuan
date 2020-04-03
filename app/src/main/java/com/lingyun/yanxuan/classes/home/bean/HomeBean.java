3b213e4d75bd0e1f0b5147ac903e6cc0490e2c3e
lanlan <lanyuzx@qq.com>
2020-04-03 10:37:15 +0800

    1

--- app/src/main/java/com/lingyun/yanxuan/classes/home/bean/HomeBean.java
+++ app/src/main/java/com/lingyun/yanxuan/classes/home/bean/HomeBean.java
@@ -3,12 +3,66 @@ package com.lingyun.yanxuan.classes.home.bean;
 import java.util.List;
 import java.util.Map;
 
+import lombok.Data;
+import lombok.ToString;
+
+@Data
 public class HomeBean {
 
     private Map commonConfigModule;
-    private List kingKongAreaV4;
-    private Map operationCfg;
-    private List hotStyleList;
+    private List<HomeKingKongAreaV4Bean> kingKongAreaV4;
+    private List<HomeOperationCfgBean> operationCfg;
+    private List<HomeHotStyleListBean> hotStyleList;
+    private  Map timePurchaseItems;
+    private Map thresholdGoodList;
+    private  List focus;
+    private List kingKongAreaV5;
+
+    @Data
+    public class HomeKingKongAreaV4Bean {
+
+     private String targetJump;
+     private Map payload;
+     private List<String> picUrls;
+     private String title;
+     private String desc;
+
+    }
+
+    @Data
+    public  class  HomeOperationCfgBean {
+        private  String targetJump;
+        private Map payload;
+        private List<String> picUrls;
+        private String title;
+    }
+
+    @Data
+    @ToString
+    public class  HomeHotStyleListBean {
+        private String bgColorTwo;
+        private String titlePic;
+        private String bgColorOne;
+        private  List<HomeHotStyleItemListBean> hotStyleItemList;
+
+        @Data
+        @ToString
+        public class   HomeHotStyleItemListBean {
+            private List itemTagList;
+            private long itemId;
+            private String picUrl;
+            private float activityPrice;
+            private  String evaluatorAvatar;
+            private String name;
+            private  int rank;
+            private String itemDesc;
+            private  float retailPrice;
+            private  String evaluatorComment;
+
+        }
+    }
+
+
 
 }
 
